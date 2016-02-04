750d8c84a6f0ecf8bb133bb930942258fef2de4f
Daan Porru <daan@wend.nl>
2016-02-04 14:43:27 +0100

    Add better error handling

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,15 @@ use std::fs::File;
 
 
 fn main() {
-    let mut f = File::open("urls.txt").unwrap();
+    let mut f = match File::open("urls.txt") {
+        Ok(f) => f,
+        Err(_) => panic!("Couldn't open file 'urls.txt'"),
+    };
     let mut s = String::new();
-    f.read_to_string(&mut s).unwrap();
+    match f.read_to_string(&mut s) {
+        Err(_) => panic!("Couldn't read file 'urls.txt'"),
+        _ => (),
+    };
 
     let sites: Vec<&str> = s.trim().split("\n").collect();
     let client = Client::new();
