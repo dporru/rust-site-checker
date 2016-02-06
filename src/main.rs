82d8fec5703e52af5aa5d834ab3fadfeab98d907
Daan Porru <daan@wend.nl>
2016-02-06 22:06:46 +0100

    Use struct to read urls.txt

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,39 @@ use hyper::Client;
 use std::fs::File;
 
 
-fn main() {
-    let mut f = match File::open("urls.txt") {
-        Ok(f) => f,
-        Err(_) => panic!("Couldn't open file 'urls.txt'"),
-    };
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Err(_) => panic!("Couldn't read file 'urls.txt'"),
-        _ => (),
-    };
+struct Sites {
+    sites: Vec<String>,
+}
 
-    let sites: Vec<&str> = s.trim().split("\n").collect();
+impl Sites {
+    fn new(file_name: &str) -> Sites {
+        let mut f = match File::open(file_name) {
+            Ok(f) => f,
+            Err(_) => panic!("Couldn't open file '{}'", file_name),
+        };
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Err(_) => panic!("Couldn't read file '{}'", file_name),
+            _ => (),
+        };
+
+        let urls: Vec<&str> = s.trim().split("\n").collect();
+        let mut sites = vec![];
+        for url in urls {
+            sites.push(url.to_string());
+        }
+        Sites {
+            sites: sites,
+        }
+    }
+}
+
+fn main() {
+    let sites = Sites::new("urls.txt");
     let client = Client::new();
 
-    for site in sites {
-         match client.get(site).send() {
+    for site in sites.sites {
+         match client.get(&site).send() {
             Ok(res) => match res.status {
                 hyper::Ok => (),
                 _ => println!("Error status for '{}': {}", site, res.status),
