e41609e1c6dce8cc713baae44f6991e9b5510e31
Daan Porru <daan@wend.nl>
2016-02-07 03:21:33 +0100

    Read url fie using function

--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,36 @@ use hyper::Client;
 use std::fs::File;
 
 
-struct Sites {
-    sites: Vec<String>,
-}
+fn get_urls(file_name: &str) -> Vec<String> {
+    let mut f = match File::open(file_name) {
+        Ok(f) => f,
+        Err(_) => panic!("Couldn't open file '{}'", file_name),
+    };
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Err(_) => panic!("Couldn't read file '{}'", file_name),
+        _ => (),
+    };
 
-impl Sites {
-    fn new(file_name: &str) -> Sites {
-        let mut f = match File::open(file_name) {
-            Ok(f) => f,
-            Err(_) => panic!("Couldn't open file '{}'", file_name),
-        };
-        let mut s = String::new();
-        match f.read_to_string(&mut s) {
-            Err(_) => panic!("Couldn't read file '{}'", file_name),
-            _ => (),
-        };
-
-        let urls: Vec<&str> = s.trim().split("\n").collect();
-        let mut sites = vec![];
-        for url in urls {
-            sites.push(url.to_string());
-        }
-        Sites {
-            sites: sites,
-        }
+    let urls: Vec<&str> = s.trim().split("\n").collect();
+    let mut new_urls = vec![];
+    for url in urls {
+        new_urls.push(url.to_string());
     }
+    new_urls
 }
 
 fn main() {
-    let sites = Sites::new("urls.txt");
+    let urls = get_urls("urls.txt");
     let client = Client::new();
 
-    for site in sites.sites {
-         match client.get(&site).send() {
+    for url in urls {
+         match client.get(&url).send() {
             Ok(res) => match res.status {
                 hyper::Ok => (),
-                _ => println!("Error status for '{}': {}", site, res.status),
+                _ => println!("Error status for '{}': {}", url, res.status),
             },
-            Err(_) => println!("Error connecting to '{}'", site)
+            Err(_) => println!("Error connecting to '{}'", url)
         };
     }
 }
